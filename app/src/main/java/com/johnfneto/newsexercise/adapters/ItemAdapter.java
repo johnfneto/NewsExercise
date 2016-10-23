170a51866d1601b979e55fd7264e1c143add229e
johnfneto <dragon64>
2016-10-23 13:42:05 +1100

    Add adapter

---
+++ app/src/main/java/com/johnfneto/newsexercise/adapters/ItemAdapter.java
@@ -0,0 +1,69 @@
+package com.johnfneto.newsexercise.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.johnfneto.newsexercise.R;
+import com.johnfneto.newsexercise.models.Items;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by johnneto on 23/10/16.
+ */
+
+public class ItemAdapter extends BaseAdapter {
+    public static final String TAG = "ItemAdapter";
+    private List<Items.Item> itemsList;
+    private static LayoutInflater inflater = null;
+    private final Context context;
+
+    public ItemAdapter(Context context, List<Items.Item> itemsList) {
+        this.context = context;
+        this.itemsList = itemsList;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        if (itemsList == null)
+            return 0;
+        else
+            return itemsList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return itemsList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi=convertView;
+        if (convertView == null) {
+            vi = inflater.inflate(R.layout.list_item, null);
+        }
+        TextView title = (TextView) vi.findViewById(R.id.title);
+        TextView description = (TextView) vi.findViewById(R.id.description);
+        ImageView image = (ImageView) vi.findViewById(R.id.image);
+
+
+        title.setText(itemsList.get(position).getTitle());
+        description.setText(itemsList.get(position).getDescription());
+
+        Picasso.with(context).load(itemsList.get(position).getImage()).into(image);
+
+        return vi;
+    }
+}
