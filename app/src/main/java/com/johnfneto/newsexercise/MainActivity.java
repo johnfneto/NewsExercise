a235b54d4bdac1f8f0124d4d1a7ed056b56c0e64
johnfneto <dragon64>
2016-10-23 12:02:43 +1100

    Add layouts

--- app/src/main/java/com/johnfneto/newsexercise/MainActivity.java
+++ app/src/main/java/com/johnfneto/newsexercise/MainActivity.java
@@ -1,13 +1,28 @@
 package com.johnfneto.newsexercise;
 
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private ListView listView;
+    private ActionBar actionBar;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = getSupportActionBar();
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        listView = (ListView) findViewById(R.id.listView);
     }
 }
